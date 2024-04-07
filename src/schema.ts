8e11819f772f9f220c75a550b3edf9d767a8adda
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-07 15:22:53 +0800

    feat: add basic schema for flashcards

--- src/schema.ts
+++ src/schema.ts
@@ -1,34 +1,84 @@
-import { createEmptyCard, FSRS, Rating, type ReviewLog } from 'ts-fsrs';
+import { relations, sql } from 'drizzle-orm';
+import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-// Schema
-
-// TODO add user and auth
+// This file contains the schema for the database.
+// Note that timestamp, boolean, and enum are not supported in SQLite.
+// The enum type is used for type inference, and not enforced in the database.
+// See https://orm.drizzle.team/docs/column-types/sqlite#text
 
 // See https://open-spaced-repetition.github.io/ts-fsrs/
-
-// TODO Maybe store the parameters for users to customise it the way they want.
+// TODO add user and auth
+// TODO Store parameters for users to allow for customisation
 // Parameters
 
-// TODO ReviewLog - handle
+export const states = ['New', 'Learning', 'Review', 'Relearning'] as const;
+export const ratings = ['Manual', 'Again', 'Hard', 'Good', 'Easy'] as const;
 
-// TODO Card
+export const reviewLogs = sqliteTable('review_logs', {
+  id: text('id').primaryKey(),
+  cardId: text('card_id').notNull(),
+  grade: text('grade', { enum: ratings }).notNull(),
+  state: text('state', { enum: states }).notNull(),
 
-// TODO CardContent - content associated with a card
-// question, answer
-// source, sourceId, deleted
+  due: integer('due', { mode: 'timestamp' }).notNull(),
+  stability: real('stability').notNull(),
+  difficulty: real('difficulty').notNull(),
+  elapsed_days: integer('elapsed_days').notNull(),
+  last_elapsed_days: integer('last_elapsed_days').notNull(),
+  scheduled_days: integer('scheduled_days').notNull(),
+  review: integer('review', { mode: 'timestamp' }).notNull(),
+  duration: integer('duration').notNull().default(0),
+  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+});
 
-// enum Rating - rating to be given to a card
+// * For now we just copy the schema from the ts-fsrs-demo example
+export const cards = sqliteTable('cards', {
+  id: text('id').primaryKey(),
+  due: integer('due', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  stability: real('stability').notNull(),
+  difficulty: real('difficulty').notNull(),
+  elapsed_days: integer('elapsed_days').notNull(),
+  scheduled_days: integer('scheduled_days').notNull(),
+  reps: integer('reps').notNull(),
+  lapses: integer('lapses').notNull(),
+  state: text('state', { enum: states }).notNull(),
+  last_review: integer('last_review', { mode: 'timestamp' }),
 
-// enum State - the current state of a card, which can be New, Learning, Review, Relearning
-// This is similar to what exists for Anki
+  suspended: integer('suspended', { mode: 'boolean' }).notNull().default(false),
 
-let card = createEmptyCard();
-const f = new FSRS({});
+  // revlogs logs
+  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+});
 
-let schedulingCards = f.repeat(card, new Date());
+export const cardContents = sqliteTable('card_contents', {
+  id: text('id').primaryKey(),
+  // card
+  cardId: text('card_id').notNull(),
 
-for (let i = 0; i < 10; i++) {
-  console.log(card.due);
-  card = schedulingCards[Rating.Good].card;
-  schedulingCards = f.repeat(card, card.due);
-}
+  question: text('question').notNull().default(''),
+  answer: text('answer').notNull().default(''),
+  source: text('source').notNull().default(''),
+  sourceId: text('sourceId'),
+  extend: text('extend', { mode: 'json' }),
+  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+});
+
+export const reviewLogsRelations = relations(reviewLogs, ({ one }) => ({
+  card: one(cards, {
+    fields: [reviewLogs.cardId],
+    references: [cards.id],
+  }),
+}));
+
+export const cardsRelations = relations(cards, ({ one, many }) => ({
+  reviewLogs: many(reviewLogs),
+}));
+
+export const cardContentsRelations = relations(cardContents, ({ one }) => ({
+  card: one(cards, {
+    fields: [cardContents.cardId],
+    references: [cards.id],
+  }),
+}));
