ace71883759197246858bbd78c79b593effdd0ac
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-10 13:39:47 +0800

    feat: add more variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,9 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        success: 'bg-success text-success-foreground hover:bg-success/90',
+        accentblue:
+          'bg-accentblue text-accentblue-foreground hover:bg-accentblue/90',
       },
       size: {
         default: 'h-10 px-4 py-2',
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,6 +49,14 @@ const config = {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))',
         },
+        success: {
+          DEFAULT: 'hsl(var(--success))',
+          foreground: 'hsl(var(--success-foreground))',
+        },
+        accentblue: {
+          DEFAULT: 'hsl(var(--accentblue))',
+          foreground: 'hsl(var(--accentblue-foreground))',
+        },
         popover: {
           DEFAULT: 'hsl(var(--popover))',
           foreground: 'hsl(var(--popover-foreground))',
