493495e426ca87577d8ffa8e475e1ba1c4e95b20
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-10 13:39:54 +0800

    chore: remove old trpc

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,4 @@
 import { Context } from '@/server/context';
-import type { AppRouter } from '@/server/routers/_app';
-import { httpBatchLink } from '@trpc/client';
-import { createTRPCNext } from '@trpc/next';
 import { initTRPC } from '@trpc/server';
 
 // See https://trpc.io/docs/quickstart#1-create-a-router-instance
@@ -18,49 +15,3 @@ const t = initTRPC.context<Context>().create({});
  */
 export const router = t.router;
 export const publicProcedure = t.procedure;
-
-function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
-
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
-
-// See https://trpc.io/docs/client/nextjs/setup#4-create-trpc-hooks
-export const trpc = createTRPCNext<AppRouter>({
-  config(opts) {
-    return {
-      links: [
-        httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/v11/ssr
-           **/
-          url: `${getBaseUrl()}/api/trpc`,
-
-          // You can pass any HTTP headers you wish here
-          async headers() {
-            return {
-              // authorization: getAuthCookie(),
-            };
-          },
-        }),
-      ],
-    };
-  },
-
-  /**
-   * @link https://trpc.io/docs/v11/ssr
-   **/
-  ssr: false,
-});
