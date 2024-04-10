ac5b2fb4175f15a3242a03b6505fbea3d70f1dff
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-10 12:46:57 +0800

    refactor: use immer for state change

--- src/hooks/card/use-grade-mutation.ts
+++ src/hooks/card/use-grade-mutation.ts
@@ -1,10 +1,17 @@
 import { getReviewDayForEachRating } from '@/utils/fsrs';
 import { ReactQueryOptions, trpc } from '@/utils/trpc';
 import { endOfDay, isBefore } from 'date-fns';
+import { produce } from 'immer';
 
 type GradeMutationOptions = ReactQueryOptions['card']['grade'];
+type GradeMutation = ReturnType<typeof trpc.card.grade.useMutation>;
 
-export function useGradeMutation(options?: GradeMutationOptions) {
+/**
+ * Hook to grade a card.
+ */
+export function useGradeMutation(
+  options?: GradeMutationOptions
+): GradeMutation {
   const utils = trpc.useUtils();
 
   return trpc.card.grade.useMutation({
@@ -12,6 +19,7 @@ export function useGradeMutation(options?: GradeMutationOptions) {
     async onMutate({ grade, id }) {
       await utils.card.all.cancel();
       const allCards = utils.card.all.getData();
+
       if (!allCards) {
         return;
       }
@@ -20,32 +28,29 @@ export function useGradeMutation(options?: GradeMutationOptions) {
       if (!card) {
         return;
       }
-
       const reviewDay = getReviewDayForEachRating(card.cards);
       const day = reviewDay[grade];
 
-      if (isBefore(day, endOfDay(new Date()))) {
-        const nextCards = allCards
-          .map((card) => {
-            if (card.cards.id === id) {
-              return {
-                ...card,
-                cards: {
-                  ...card.cards,
-                  due: day.toISOString(),
-                },
-              };
-            }
+      const cardToBeReviewedToday = isBefore(day, endOfDay(new Date()));
 
-            return card;
-          })
-          .sort((a, b) => (isBefore(a.cards.due, b.cards.due) ? -1 : 1));
+      const nextCards = produce(allCards, (draft) => {
+        const cardIndex = draft.findIndex((card) => card.cards.id === id);
+        const cardNotFound = cardIndex === -1;
+        if (cardNotFound) return;
 
-        utils.card.all.setData(undefined, nextCards);
-      } else {
-        const nextCards = allCards.filter((card) => card.cards.id !== id);
-        utils.card.all.setData(undefined, nextCards);
-      }
+        if (cardToBeReviewedToday) {
+          // We update the due date of the card
+          draft[cardIndex].cards.due = day.toISOString();
+          draft.sort((a, b) => (isBefore(a.cards.due, b.cards.due) ? -1 : 1));
+          return;
+        }
+
+        // We're allowed to return in Immer
+        // https://immerjs.github.io/immer/return
+        return draft.filter((card) => card.cards.id !== id);
+      });
+
+      utils.card.all.setData(undefined, nextCards);
     },
   });
 }
