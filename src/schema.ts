03b76e93f05a8c27e7addfe3a93308e2eb00f9c1
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-13 23:36:05 +0800

    feat: add decks to schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
-import { relations, sql } from 'drizzle-orm';
-import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { relations, sql } from "drizzle-orm";
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 // This file contains the schema for the database.
 // Note that timestamp, boolean, and enum are not supported in SQLite.
@@ -11,71 +11,78 @@ import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 // TODO Store parameters for users to allow for customisation
 // Parameters
 
-export const states = ['New', 'Learning', 'Review', 'Relearning'] as const;
+export const states = ["New", "Learning", "Review", "Relearning"] as const;
 export type State = (typeof states)[number];
 
-export const ratings = ['Manual', 'Again', 'Hard', 'Good', 'Easy'] as const;
+export const ratings = ["Manual", "Again", "Hard", "Good", "Easy"] as const;
 export type Rating = (typeof ratings)[number];
 
-export const reviewLogs = sqliteTable('review_logs', {
-  id: text('id').primaryKey(),
-  cardId: text('card_id').notNull(),
-  grade: text('grade', { enum: ratings }).notNull(),
-  state: text('state', { enum: states }).notNull(),
+export const reviewLogs = sqliteTable("review_logs", {
+  id: text("id").primaryKey(),
+  cardId: text("card_id").notNull(),
+  grade: text("grade", { enum: ratings }).notNull(),
+  state: text("state", { enum: states }).notNull(),
 
-  due: integer('due', { mode: 'timestamp' }).notNull(),
-  stability: real('stability').notNull(),
-  difficulty: real('difficulty').notNull(),
-  elapsed_days: integer('elapsed_days').notNull(),
-  last_elapsed_days: integer('last_elapsed_days').notNull(),
-  scheduled_days: integer('scheduled_days').notNull(),
-  review: integer('review', { mode: 'timestamp' }).notNull(),
-  duration: integer('duration').notNull().default(0),
-  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+  due: integer("due", { mode: "timestamp" }).notNull(),
+  stability: real("stability").notNull(),
+  difficulty: real("difficulty").notNull(),
+  elapsed_days: integer("elapsed_days").notNull(),
+  last_elapsed_days: integer("last_elapsed_days").notNull(),
+  scheduled_days: integer("scheduled_days").notNull(),
+  review: integer("review", { mode: "timestamp" }).notNull(),
+  duration: integer("duration").notNull().default(0),
+  deleted: integer("deleted", { mode: "boolean" }).notNull().default(false),
 });
 
 export type ReviewLog = typeof reviewLogs.$inferSelect;
 export type NewReviewLog = typeof reviewLogs.$inferInsert;
 
 // * For now we just copy the schema from the ts-fsrs-demo example
-export const cards = sqliteTable('cards', {
-  id: text('id').primaryKey(),
-  due: integer('due', { mode: 'timestamp' })
+export const cards = sqliteTable("cards", {
+  id: text("id").primaryKey(),
+  due: integer("due", { mode: "timestamp" })
     .notNull()
     .default(sql`(unixepoch())`), // https://orm.drizzle.team/learn/guides/timestamp-default-value
-  stability: real('stability').notNull(),
-  difficulty: real('difficulty').notNull(),
-  elapsed_days: integer('elapsed_days').notNull(),
-  scheduled_days: integer('scheduled_days').notNull(),
-  reps: integer('reps').notNull(),
-  lapses: integer('lapses').notNull(),
-  state: text('state', { enum: states }).notNull(),
-  last_review: integer('last_review', { mode: 'timestamp' }),
+  stability: real("stability").notNull(),
+  difficulty: real("difficulty").notNull(),
+  elapsed_days: integer("elapsed_days").notNull(),
+  scheduled_days: integer("scheduled_days").notNull(),
+  reps: integer("reps").notNull(),
+  lapses: integer("lapses").notNull(),
+  state: text("state", { enum: states }).notNull(),
+  last_review: integer("last_review", { mode: "timestamp" }),
 
-  suspended: integer('suspended', { mode: 'boolean' }).notNull().default(false),
+  suspended: integer("suspended", { mode: "boolean" }).notNull().default(false),
 
   // revlogs logs
-  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+  deleted: integer("deleted", { mode: "boolean" }).notNull().default(false),
 });
 
 export type Card = typeof cards.$inferSelect;
 export type NewCard = typeof cards.$inferInsert;
 
-export const cardContents = sqliteTable('card_contents', {
-  id: text('id').primaryKey(),
+// TODO rename to camelCase
+export const cardContents = sqliteTable("card_contents", {
+  id: text("id").primaryKey(),
   // card
-  cardId: text('card_id').notNull(),
+  cardId: text("card_id").notNull(),
 
-  question: text('question').notNull().default(''),
-  answer: text('answer').notNull().default(''),
-  source: text('source').notNull().default(''),
-  sourceId: text('sourceId'),
-  extend: text('extend', { mode: 'json' }),
-  deleted: integer('deleted', { mode: 'boolean' }).notNull().default(false),
+  question: text("question").notNull().default(""),
+  answer: text("answer").notNull().default(""),
+  source: text("source").notNull().default(""),
+  sourceId: text("sourceId"),
+  extend: text("extend", { mode: "json" }),
+  deleted: integer("deleted", { mode: "boolean" }).notNull().default(false),
 });
 
 export type CardContent = typeof cardContents.$inferSelect;
 export type NewCardContent = typeof cardContents.$inferInsert;
+
+export const decks = sqliteTable("decks", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  deleted: integer("deleted", { mode: "boolean" }).notNull().default(false),
+});
 
 export const reviewLogsRelations = relations(reviewLogs, ({ one }) => ({
   card: one(cards, {
@@ -86,6 +93,7 @@ export const reviewLogsRelations = relations(reviewLogs, ({ one }) => ({
 
 export const cardsRelations = relations(cards, ({ one, many }) => ({
   reviewLogs: many(reviewLogs),
+  decks: many(decks),
 }));
 
 export const cardContentsRelations = relations(cardContents, ({ one }) => ({
@@ -93,4 +101,8 @@ export const cardContentsRelations = relations(cardContents, ({ one }) => ({
     fields: [cardContents.cardId],
     references: [cards.id],
   }),
+}));
+
+export const decksRelations = relations(decks, ({ many }) => ({
+  cards: many(cards),
 }));
