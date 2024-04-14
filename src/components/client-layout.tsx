ad0024a1a8cb209ea8b6ce4463ce56c39aa778cc
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-14 21:43:18 +0800

    fix: resolve type error with trpc

--- src/components/client-layout.tsx
+++ src/components/client-layout.tsx
@@ -2,20 +2,12 @@
 // See https://stackoverflow.com/questions/74992326/does-use-client-in-next-js-13-root-layout-make-whole-routes-client-component
 
 import { NavigationBar } from "@/components/nav-bar";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { trpc } from "@/utils/trpc";
-import { cn } from "@/utils/ui";
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
+import { PropsWithChildren } from "react";
 
-// See https://ui.shadcn.com/docs/installation/next
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-function ClientLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+function ClientLayout({ children }: PropsWithChildren<{}>) {
   return (
     <ThemeProvider
       attribute="class"
@@ -23,19 +15,12 @@ function ClientLayout({
       enableSystem
       disableTransitionOnChange
     >
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable,
-        )}
-      >
-        <NavigationBar />
-        {children}
-        <Toaster />
-      </body>
+      <NavigationBar />
+      {children}
+      <Toaster />
     </ThemeProvider>
   );
 }
 
 // See https://trpc.io/docs/client/nextjs/setup#5-configure-_apptsx
-export default trpc.withTRPC(ClientLayout);
+export default trpc.withTRPC(ClientLayout) as typeof ClientLayout;
