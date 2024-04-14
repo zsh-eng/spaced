f1b0c68b26b0b63e3beb762a6d6fafca76dc9ff8
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-14 11:28:44 +0800

    feat: add hooks for create delete deck

--- src/utils/deck.ts
+++ src/utils/deck.ts
@@ -1,8 +1,15 @@
 import { NewDeck } from "@/schema";
 
-export function newDeck({ name }: { name: string }): NewDeck {
+export function newDeck({
+  name,
+  description,
+}: {
+  name: string;
+  description?: string;
+}): NewDeck {
   return {
     id: crypto.randomUUID(),
     name,
+    description: description ?? "",
   };
 }
