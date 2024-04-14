be9d0ebbac364281de87e40d6fb069056aafe55e
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-14 14:12:20 +0800

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,13 @@
-"use client";
 // See https://stackoverflow.com/questions/74992326/does-use-client-in-next-js-13-root-layout-make-whole-routes-client-component
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import ClientLayout from "@/components/client-layout";
+import { Metadata } from "next";
 import "./globals.css";
-import { cn } from "@/utils/ui";
-import { Toaster } from "@/components/ui/sonner";
-import { trpc } from "@/utils/trpc";
-import { NavigationBar } from "@/components/nav-bar";
 
-// See https://ui.shadcn.com/docs/installation/next
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-// TODO handle metadata with tRPC and Next.js
-// export const metadata: Metadata = {
-//   title: 'Spaced',
-//   description: 'A better way of learning.',
-// };
+export const metadata: Metadata = {
+  title: "Spaced",
+  description: "A better way of learning.",
+};
 
 function RootLayout({
   children,
@@ -25,19 +16,10 @@ function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable,
-        )}
-      >
-        <NavigationBar />
-        {children}
-        <Toaster />
-      </body>
+      <ClientLayout>{children}</ClientLayout>
     </html>
   );
 }
 
 // See https://trpc.io/docs/client/nextjs/setup#5-configure-_apptsx
-export default trpc.withTRPC(RootLayout);
+export default RootLayout;
---
+++ src/components/client-layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+// See https://stackoverflow.com/questions/74992326/does-use-client-in-next-js-13-root-layout-make-whole-routes-client-component
+
+import { NavigationBar } from "@/components/nav-bar";
+import { Toaster } from "@/components/ui/sonner";
+import { trpc } from "@/utils/trpc";
+import { cn } from "@/utils/ui";
+import { Inter } from "next/font/google";
+
+// See https://ui.shadcn.com/docs/installation/next
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+function ClientLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <body
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        inter.variable,
+      )}
+    >
+      <NavigationBar />
+      {children}
+      <Toaster />
+    </body>
+  );
+}
+
+// See https://trpc.io/docs/client/nextjs/setup#5-configure-_apptsx
+export default trpc.withTRPC(ClientLayout);
