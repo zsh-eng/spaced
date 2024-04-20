45472bd0b17482b06368dd24b2a879f486e28a42
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-20 12:06:54 +0800

    feat: add generic form inputs

---
+++ src/components/form/form-input.tsx
@@ -0,0 +1,52 @@
+import { FormInputProps } from "@/components/form/input.types";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FieldValues } from "react-hook-form";
+
+type FormTextInputProps<TFieldValues extends FieldValues> =
+  FormInputProps<TFieldValues> & {
+    placeholder?: string;
+    description?: string;
+    className?: string;
+  };
+
+export function FormTextInput<TFieldValues extends FieldValues>({
+  form,
+  name,
+  label,
+  disabled,
+  placeholder,
+  description,
+  className,
+}: FormTextInputProps<TFieldValues>) {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+          <FormControl>
+            <Input
+              className={className}
+              placeholder={placeholder}
+              disabled={disabled}
+              {...field}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+FormTextInput.displayName = "FormTextInput";
