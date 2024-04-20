0994ccdac3d14d45f8b4c136ecdb7bd76c087877
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-20 13:53:00 +0800

    refactor: use form for decks

--- src/form.ts
+++ src/form.ts
@@ -1,3 +1,4 @@
+import { RouterInputs } from "@/utils/trpc";
 import { z } from "zod";
 
 const MAX_INPUT_LENGTH = 20000;
@@ -27,3 +28,24 @@ export const cardContentDefaultValues = {
   question: "",
   answer: "",
 } satisfies CardContentFormValues;
+
+export const deckFormSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Name is required.",
+    })
+    .max(50, {
+      message: "Name is too long.",
+    }),
+  description: z.string().max(MAX_INPUT_LENGTH, {
+    message: "Description is too long.",
+  }),
+});
+
+export type DeckFormValues = z.infer<typeof deckFormSchema>;
+
+export const deckDefaultValues = {
+  name: "",
+  description: "",
+} satisfies DeckFormValues;
