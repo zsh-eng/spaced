474919f69dbdd684562f19bcf17c4fbc2b19250f
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-27 11:34:37 +0800

    refactor: clean up nav bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -25,7 +25,6 @@ import { useSession } from "next-auth/react";
 
 // TODO This nav menu is a bit of a mess, we should extract the links
 // And refactor it
-
 // For mobile navigation
 function MenuDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -88,6 +87,32 @@ function MenuDrawer() {
     </Drawer>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
 
 export function NavigationBar() {
   const session = useSession();
@@ -159,28 +184,4 @@ export function NavigationBar() {
   );
 }
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
+NavigationBar.displayName = "NavigationBar";
