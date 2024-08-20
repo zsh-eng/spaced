bc14dabd31ba90ebf2ac4003e7fcf46680c7f2e2
zsheng <chengzhisheng2001@gmail.com>
2024-08-21 00:07:42 +0800

    feat: update design of deck actions

--- src/components/decks/deck-cards.tsx
+++ src/components/decks/deck-cards.tsx
@@ -8,15 +8,9 @@ import { useDeleteDeck } from "@/hooks/deck/use-delete-deck";
 import { trpc } from "@/utils/trpc";
 import { cn } from "@/utils/ui";
 import { format } from "date-fns";
-import {
-  ArrowLeft,
-  Clock,
-  Diamond,
-  Loader2,
-  NotebookTabs,
-  Trash,
-} from "lucide-react";
+import { ArrowLeft, Clock, Loader2, NotebookTabs, Trash } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type Props = {
   deckId: string;
@@ -57,7 +51,8 @@ export default function DeckCards({ deckId }: Props) {
   );
   const { data: deckData, isLoading: isDeckLoading } = trpc.deck.all.useQuery();
   const deck = deckData?.find((d) => d.id === deckId);
-  const { mutateAsync: deleteDeck, isPending: isDeleting } = useDeleteDeck();
+  const { mutate: deleteDeck, isPending: isDeleting } = useDeleteDeck();
+  const router = useRouter();
 
   if (isLoading || isDeckLoading || !data) {
     return (
@@ -85,34 +80,33 @@ export default function DeckCards({ deckId }: Props) {
       <Title title={deck.name} />
       <section className="col-span-12 mb-6 flex w-full flex-col gap-y-4 pl-2">
         <div className="flex gap-x-4">
-          <p className="flex items-center text-xl">
-            <NotebookTabs className="mr-2 h-6 w-6" />
+          <p className="flex items-center text-lg">
+            <NotebookTabs className="mr-2 h-5 w-5" />
             {deck.cardCount}
           </p>
-          <p className="flex items-center text-xl">
-            <Clock className="mr-2 h-6 w-6" />
+          <p className="flex items-center text-lg">
+            <Clock className="mr-2 h-5 w-5" />
             {format(deck.createdAt, "MMM d, yyyy")}
           </p>
-          <p>
-            <Button
-              size={"icon"}
-              variant={"ghost"}
-              className="mr-2"
-              onClick={async () => {
-                // After deleting, we should go to the decks page
-                // This should be a confirmation dialog
-                return deleteDeck({
-                  deckId: deck.id,
-                });
-              }}
-            >
-              {isDeleting ? (
-                <Loader2 className="h-4 w-4 animate-spin" />
-              ) : (
-                <Trash className="" />
-              )}
-            </Button>
-          </p>
+          <Button
+            size={"icon"}
+            variant={"ghost"}
+            className="ml-auto"
+            onClick={async () => {
+              // After deleting, we should go to the decks page
+              // This should be a confirmation dialog
+              deleteDeck({
+                deckId: deck.id,
+              });
+              router.push("/decks");
+            }}
+          >
+            {isDeleting ? (
+              <Loader2 className="h-4 w-4 animate-spin" />
+            ) : (
+              <Trash className="" />
+            )}
+          </Button>
         </div>
       </section>
       {data.pages.map((group, i) => {
